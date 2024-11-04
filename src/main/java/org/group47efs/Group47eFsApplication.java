89cdf660bba2f8b9ae5b69c2bbf068b304da9784
AlexAlpine <alex6122223@gmail.com>
2024-11-04 20:38:08 +0100

    first commit

---
+++ src/main/java/org/group47efs/Group47eFsApplication.java
@@ -0,0 +1,13 @@
+package org.group47efs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Group47eFsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Group47eFsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/group47efs/Group47eFsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.group47efs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Group47eFsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
